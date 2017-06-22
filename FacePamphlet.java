c156686cab56847ddac8ab5284c53f2e68b56fd2
S <s>
2017-06-22 17:18:02 -0400

    {"types":"C","runs":6}

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -1,3 +1,4 @@
+
 /* 
  * File: FacePamphlet.java
  * -----------------------
@@ -11,66 +12,106 @@ import acm.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class FacePamphlet extends Program 
-					implements FacePamphletConstants {
+public class FacePamphlet extends ConsoleProgram implements FacePamphletConstants {
+
+	private JTextField nameField;
+	private JButton add;
+	private JButton delete;
+	private JButton lookUp;
+	private JTextField statusTextField;
+	private JButton statusButton;
+	private JTextField pictureTextField;
+	private JButton pictureButton;
+	private JTextField addFriendTextField;
+	private JButton addFriendButton;
 
 	/**
-	 * This method has the responsibility for initializing the 
-	 * interactors in the application, and taking care of any other 
-	 * initialization that needs to be performed.
+	 * This method has the responsibility for initializing the interactors in
+	 * the application, and taking care of any other initialization that needs
+	 * to be performed.
 	 */
 	public void init() {
-		
+
 		JLabel name = new JLabel("Name:");
-		add(name,NORTH);
-		
-		JTextField nameField = new JTextField(TEXT_FIELD_SIZE);
-		add(nameField,NORTH);
-		
-		JButton add = new JButton("Add");
-		add(add,NORTH);
-		
-		JButton delete = new JButton("Delete");
-		add(delete,NORTH);
-		
-		JButton lookUp = new JButton("Lookup");
-		add(lookUp,NORTH);
-		
-		JTextField statusTextField = new JTextField(TEXT_FIELD_SIZE);
-		add(statusTextField,WEST);
-		
-		JButton statusButton = new JButton("Change Status");
-		add(statusButton,WEST);
-		
+		add(name, NORTH);
+
+		nameField = new JTextField(TEXT_FIELD_SIZE);
+		add(nameField, NORTH);
+
+		add = new JButton("Add");
+		add(add, NORTH);
+
+		delete = new JButton("Delete");
+		add(delete, NORTH);
+
+		lookUp = new JButton("Lookup");
+		add(lookUp, NORTH);
+
+		statusTextField = new JTextField(TEXT_FIELD_SIZE);
+		add(statusTextField, WEST);
+
+		statusButton = new JButton("Change Status");
+		add(statusButton, WEST);
+
 		JLabel blank1 = new JLabel(EMPTY_LABEL_TEXT);
-		add(blank1,WEST);
-		
-		JTextField pictureTextField = new JTextField(TEXT_FIELD_SIZE);
-		add(pictureTextField,WEST);
-		
-		JButton pictureButton = new JButton("Change Picture");
-		add(pictureButton,WEST);
-		
+		add(blank1, WEST);
+
+		pictureTextField = new JTextField(TEXT_FIELD_SIZE);
+		add(pictureTextField, WEST);
+
+		pictureButton = new JButton("Change Picture");
+		add(pictureButton, WEST);
+
 		JLabel blank2 = new JLabel(EMPTY_LABEL_TEXT);
-		add(blank2,WEST);
-		
-		JTextField addFriendTextField = new JTextField(TEXT_FIELD_SIZE);
-		add(addFriendTextField,WEST);
-		
-		JButton addFriendButton = new JButton("Add Friend");
-		add(addFriendButton,WEST);
-		
-		
-    }
-    
-  
-    /**
-     * This class is responsible for detecting when the buttons are
-     * clicked or interactors are used, so you will have to add code
-     * to respond to these actions.
-     */
-    public void actionPerformed(ActionEvent e) {
-		// You fill this in as well as add any additional methods
+		add(blank2, WEST);
+
+		addFriendTextField = new JTextField(TEXT_FIELD_SIZE);
+		add(addFriendTextField, WEST);
+
+		addFriendButton = new JButton("Add Friend");
+		add(addFriendButton, WEST);
+
+		addActionListeners();
+
+	}
+
+	/**
+	 * This class is responsible for detecting when the buttons are clicked or
+	 * interactors are used, so you will have to add code to respond to these
+	 * actions.
+	 */
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == nameField) {
+			println(nameField.getText());
+		}
+		if (e.getSource() == add) {
+			println(nameField.getText());
+		}
+		if (e.getSource() == delete) {
+			println(nameField.getText()+" was deleted.");
+		}
+		if (e.getSource() == lookUp) {
+			println("Looking up: "+nameField.getText());
+		}
+		if (e.getSource() == statusTextField) {
+			println(nameField.getText() + " is currentyly " + statusTextField.getText());
+		}
+		if (e.getSource() == statusButton) {
+			println(nameField.getText() + " is currentyly " + statusTextField.getText());
+		}
+		if (e.getSource() == pictureTextField) {
+			println(pictureTextField.getText() + " Picture added.");
+		}
+		if (e.getSource() == pictureButton) {
+			println(nameField.getText() + " is currentyly " + statusTextField.getText());
+		}
+		if (e.getSource() == addFriendTextField) {
+			println(addFriendTextField+ "was added as a friend!");
+		}
+		if (e.getSource() == addFriendButton) {
+			println(addFriendTextField+ "was added as a friend!");
+		}
 	}
 
 }
